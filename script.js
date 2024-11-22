bf253cda0b1c03d4e94ffa9f51245a5981abea51
AkbarGZ <akbarsudahsembuh4@gmail.com>
2024-11-22 18:39:25 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function generateNumber() {
-    // Generate a random number between 1 and 100
-    const randomNum = Math.floor(Math.random() * 100) + 1;
-    
-    // Display the random number on the page
-    document.getElementById('number-display').textContent = randomNum;
-}
